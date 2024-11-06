72ccf790a50fb54033abd5536910a8c0d5cf2977
Arif CHAIN Roger <arif2766313@gmail.com>
2024-11-06 21:40:58 +0700

    Competition 4 (Complete File)

---
+++ Comp4_Annotated(Final)(cpp).ino
@@ -0,0 +1,117 @@
+#include <ESP8266WiFi.h>  // ไลบรารีสำหรับเชื่อมต่อ WiFi
+#include <Servo.h>  // ไลบรารีสำหรับควบคุม Servo motor
+
+const char* ssid = "Nasiminyak";  // ชื่อเครือข่าย WiFi
+const char* password = "0973453602";  // รหัสผ่าน WiFi
+const int port = 8888;  // พอร์ตสำหรับเชื่อมต่อ
+
+WiFiServer server(port);  // สร้างเซิร์ฟเวอร์ WiFi
+Servo myServoX;  // สร้างวัตถุ Servo สำหรับแกน X
+Servo myServoY;  // สร้างวัตถุ Servo สำหรับแกน Y
+Servo myServoZ;  // สร้างวัตถุ Servo สำหรับแกน Z
+
+void setup() {
+  myServoX.attach(2);  // กำหนดขา D4 สำหรับ Servo X
+  myServoY.attach(0);  // กำหนดขา D3 สำหรับ Servo Y
+  myServoZ.attach(5);  // กำหนดขา D1 สำหรับ Servo Z
+  Serial.begin(9600);  // เริ่มการสื่อสารผ่าน Serial port
+  WiFi.begin(ssid, password);  // เริ่มการเชื่อมต่อ WiFi
+
+  while (WiFi.status() != WL_CONNECTED) {  // รอจนกว่าเชื่อมต่อ WiFi สำเร็จ
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  Serial.println("Connected to WiFi");  // แสดงข้อความเมื่อเชื่อมต่อ WiFi สำเร็จ
+  Serial.println(WiFi.localIP());  // แสดง IP ของ ESP8266
+  //myServoX.write(90);
+  //delay(1000);
+  //myServoY.write(90);
+  //delay(1000);
+  //myServoZ.write(0);
+  //delay(1000);
+
+  server.begin();  // เริ่มเซิร์ฟเวอร์ WiFi
+}
+
+unsigned long previousMillis = 0;  // เก็บค่าเวลาล่าสุดที่ใช้ในการหมุน Servo
+const long interval = 10;  // ระยะเวลาระหว่างการหมุน Servo (100 milliseconds)
+
+void loop() {
+  WiFiClient client = server.available();  // รอการเชื่อมต่อจากไคลเอนต์
+
+  if (client) {
+    Serial.println("Client connected");
+    client.setTimeout(0);
+    Serial.print("กำลังเซ็ตแกน...");
+    myServoX.write(90);  // ตั้งค่า Servo X ไปที่ 90 องศา
+    delay(1000);
+    myServoY.write(90);  // ตั้งค่า Servo Y ไปที่ 90 องศา
+    delay(1000);
+    myServoZ.write(0);  // ตั้งค่า Servo Z ไปที่ 0 องศา
+    delay(1000);
+    Serial.println("เซ็ตสำเร็จ");
+
+    while (client.connected()) {  // ตรวจสอบว่าคลายเอนต์ยังเชื่อมต่ออยู่หรือไม่
+      if (client.available() > 0) {  // ตรวจสอบว่ามีข้อมูลจากคลายเอนต์
+        int x = client.parseInt();  // รับค่าพิกัด x จาก Python script
+        client.read();  // อ่านตัวคั่น (',')
+        int y = client.parseInt();  // รับค่าพิกัด y จาก Python script
+        Serial.print("X = ");
+        Serial.print(x);
+        Serial.print(", Y = ");
+        Serial.println(y);
+
+        if (x < 200) {  // ถ้าค่าพิกัด x น้อยกว่า 200
+          myServoX.attach(2);
+          unsigned long currentMillis = millis();  // จับเวลาปัจจุบัน
+          if (currentMillis - previousMillis >= interval) {
+            Serial.println("หันซ้าย");
+            myServoX.write(myServoX.read() + 3);  // เพิ่มค่า angle ขึ้น 3
+            previousMillis = currentMillis;  // บันทึกค่าเวลาปัจจุบันเป็นค่าเวลาล่าสุด
+          }
+        } else if (x > 250) {  // ถ้าค่าพิกัด x มากกว่า 250
+          myServoX.attach(2);
+          unsigned long currentMillis = millis();  // จับเวลาปัจจุบัน
+          if (currentMillis - previousMillis >= interval) {
+            Serial.println("หันขวา");
+            myServoX.write(myServoX.read() - 3);  // ลดค่า angle ลง 3
+            previousMillis = currentMillis;  // บันทึกค่าเวลาปัจจุบันเป็นค่าเวลาล่าสุด
+          }
+        }
+        
+        // ควบคุมแกน y
+        if (y > 200) {  // ถ้าค่าพิกัด y มากกว่า 200
+          myServoY.detach();  // ถอด Servo motor ของแกน y
+          unsigned long currentMillis = millis();  // จับเวลาปัจจุบัน
+          if (currentMillis - previousMillis >= interval) {
+            Serial.println("หันลง");
+            myServoY.attach(0);  // Attach Servo motor ของแกน y กลับ
+            myServoY.write(myServoY.read() + 1);  // เพิ่มค่า angle ขึ้น 1
+            previousMillis = currentMillis;  // บันทึกค่าเวลาปัจจุบันเป็นค่าเวลาล่าสุด
+          }
+        } else if (y < 180) {  // ถ้าค่าพิกัด y น้อยกว่า 180
+          myServoY.detach();  // ถอด Servo motor ของแกน y
+          unsigned long currentMillis = millis();  // จับเวลาปัจจุบัน
+          if (currentMillis - previousMillis >= interval) {
+            Serial.println("หันขึ้น");
+            myServoY.attach(0);  // Attach Servo motor ของแกน y กลับ
+            myServoY.write(myServoY.read() - 1);  // ลดค่า angle ลง 1
+            previousMillis = currentMillis;  // บันทึกค่าเวลาปัจจุบันเป็นค่าเวลาล่าสุด
+          }
+        } else {  // ถ้าค่าพิกัด x และ y อยู่ในช่วงที่กำหนด
+          myServoZ.attach(5);  // Attach Servo motor ของแกน z
+          Serial.println("ยิง!!");
+          myServoZ.write(180);  // หมุน Servo ไปที่ 180 องศา
+          delay(500);
+          myServoZ.write(180);
+          delay(100);
+          myServoZ.write(0);  // หมุน Servo กลับไปที่ 0 องศา
+          delay(500);
+
+          //myServoZ.detach();
+        }
+      }
+    }
+  }
+}
---
+++ comp4_annotated(Final)(py).py
@@ -0,0 +1,71 @@
+import cv2  # ไลบรารีสำหรับการประมวลผลภาพ
+from yolo_predictions import YOLO_Pred  # โมดูลสำหรับทำการคาดการณ์ด้วย YOLO
+from parinya import LINE  # โมดูลสำหรับส่งข้อความผ่าน LINE
+import socket  # ไลบรารีสำหรับการเชื่อมต่อ socket
+import time  # ไลบรารีสำหรับจัดการเวลา
+
+# กำหนดค่า IP และ port ของ Arduino
+arduino_ip = "192.168.164.174"
+arduino_port = 8888
+# สร้าง socket สำหรับการเชื่อมต่อ
+arduino_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+arduino_socket.connect((arduino_ip, arduino_port))
+
+# โหลดไฟล์โมเดลและการตั้งค่าสำหรับ YOLO
+yolo = YOLO_Pred('my_obj.onnx', 'my_obj.yaml')
+
+# เซต token สำหรับส่งข้อความผ่าน LINE
+line = LINE('P4UjVDP6XuQ7IrigTjjtxnMOHZMaaYRJJ9svEKZ20Pp')
+
+# เปิดการทำงานของกล้อง
+cap = cv2.VideoCapture(0)
+# ส่งข้อความแจ้งเตือนเมื่อเปิดใช้งานเครื่อง
+line.sendtext('เปิดใช้งานเครื่อง')
+
+# ตัวแปรสำหรับจัดการเวลา
+last_arduino_send_time = time.perf_counter()
+last_line_send_time = time.perf_counter()
+
+while True:
+    ret, img = cap.read()  # อ่านภาพจากกล้อง
+    img = cv2.flip(img, -1)  # พลิกภาพ
+
+    key = cv2.waitKey(1)
+    if (key == ord('q')) or (ret == False):  # ถ้ากด 'q' หรือไม่สามารถอ่านภาพได้
+        break
+
+    pred_image, obj_box = yolo.predictions(img)  # ทำการคาดการณ์ด้วย YOLO
+
+    # วาดเส้นแบ่งแนวตั้งและแนวนอน
+    cv2.line(pred_image, (img.shape[1] // 2, 0), (img.shape[1] // 2, img.shape[0]), (255, 255, 0), 1)
+    cv2.line(pred_image, (0, img.shape[0] // 2), (img.shape[1], img.shape[0] // 2), (255, 255, 0), 1)
+
+    # คำนวนพิกัด x y
+    height, width, _ = pred_image.shape
+    center_x, center_y = width // 2, height // 2
+
+    # วาดจุดเล็งเป้า
+    cv2.circle(pred_image, (center_x, center_y), 5, (0, 255, 0), -2)
+
+    cv2.imshow('pred_image', pred_image)  # แสดงภาพที่ทำการคาดการณ์
+
+    if obj_box:  # ถ้ามีวัตถุที่ตรวจพบ
+        current_time = time.perf_counter()
+
+        # ส่งข้อมูลไปยัง Arduino ทุก 0.1 วินาที
+        if current_time - last_arduino_send_time >= 0.1:
+            for box in obj_box:
+                x, y, *_ = box  # ดึงพิกัด x, y จากกล่องที่ตรวจพบ
+                data = f"{x},{y}"  # รวมข้อมูลเป็นรูปแบบ "x,y"
+                print(f'Send x: {x}, y: {y}')  # แสดงพิกัดใน Python
+                arduino_socket.send(data.encode('utf-8'))  # ส่งข้อมูลไปยัง Arduino
+                last_arduino_send_time = current_time
+
+        # ส่งภาพไปยัง LINE ทุก 30 วินาที
+        if current_time - last_line_send_time >= 10:
+            line.sendimage(img[:, :, ::-1])  # ส่งภาพไปยัง LINE
+            last_line_send_time = current_time
+
+cv2.destroyAllWindows()  # ปิดหน้าต่างแสดงภาพ
+cap.release()  # ปิดการทำงานของกล้อง
+arduino_socket.close()  # ปิดการเชื่อมต่อกับ Arduino
